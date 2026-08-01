4cbc9abdb4983c9ef55f7244f746f1652d33a97c
Pawan kumar Yadav <pawan902670@gmail.com>
2026-08-01 15:58:25 +0530

    Update

--- Node_Js/Code_section/server.js
+++ Node_Js/Code_section/server.js
@@ -4,3 +4,10 @@ http.createServer((req,resp)=>{
     resp.write("<h1>Yes Your  Brother is  Here</h1>");
 resp.end("Hello");
 }).listen(4800);
+
+const https=require('http');
+https.createServer((req,resp)=>{
+    // resp.write("These side Pawan");
+    resp.write("<h2>Yes Your  Brother is  Here 2</h2>");
+resp.end("Hello");
+}).listen(4500);
