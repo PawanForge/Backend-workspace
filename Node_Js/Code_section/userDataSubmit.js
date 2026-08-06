6e6b35c2126db2451df6151ecff67a2f307ba5ed
Pawan kumar Yadav <pawan902670@gmail.com>
2026-08-06 18:02:42 +0530

    Adding new Concept

--- Node_Js/Code_section/userDataSubmit.js
+++ Node_Js/Code_section/userDataSubmit.js
@@ -1,6 +1,28 @@
-function userDataSubmit(req,resp){
-    resp.write(`
-        <h1>Hello </h1>
-        `)
+const queryString = require('querystring');
+
+function userDataSubmit(req, resp) {
+    let dataBody = [];
+
+    req.on('data', (chunk) => {
+        dataBody.push(chunk);
+    });
+
+    req.on('end', () => {
+        let rawData = Buffer.concat(dataBody).toString();
+
+        let readableData = queryString.parse(rawData);
+
+        let dataString = "My name is " + readableData.name;
+
+        console.log(dataString);
+
+        resp.write(`
+            <h1>Hello ${readableData.name}</h1>
+            <p>${dataString}</p>
+        `);
+
+        resp.end();
+    });
 }
-module.exports=userDataSubmit;
+
+module.exports = userDataSubmit;
