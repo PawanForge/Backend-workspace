8f5fe4fda9261327c06f54b755bac7dbdd467e63
Pawan kumar Yadav <pawan902670@gmail.com>
2026-08-26 13:51:25 +0530

    Deleted the data

---
+++ Express/Code/Delete_Data_DB.js
@@ -0,0 +1,131 @@
+import express from "express"; 
+import { MongoClient, ObjectId } from "mongodb"; 
+ 
+const app = express(); 
+ 
+const dbName = "pawan"; 
+const url = "mongodb://localhost:27017"; 
+ 
+const client = new MongoClient(url); 
+ 
+app.set("view engine", "ejs"); 
+ 
+app.use(express.json()); 
+ 
+// To read POST form data 
+app.use(express.urlencoded({ extended: true })); 
+ 
+client.connect().then((connection) => { 
+ 
+    const db = connection.db(dbName); 
+ 
+    // API 
+    app.get("/api", async (req, resp) => { 
+ 
+        const collection = db.collection("my_information"); 
+ 
+        const result = await collection.find().toArray(); 
+ 
+        resp.send(result); 
+    }); 
+ 
+    // UI 
+    app.get("/ui", async (req, resp) => { 
+ 
+        const collection = db.collection("my_information"); 
+ 
+        const result = await collection.find().toArray(); 
+ 
+        resp.render("student", { result }); 
+    }); 
+ 
+    // Form 
+    app.get("/add", (req, resp) => { 
+ 
+        resp.send(` 
+            <form action="/add-student" method="POST"> 
+ 
+                <input  
+                    type="text"  
+                    name="name"  
+                    placeholder="Enter the Name" 
+                /> 
+ 
+                <br/><br/> 
+ 
+                <input  
+                    type="email"  
+                    name="email"  
+                    placeholder="Enter the Email" 
+                /> 
+ 
+                <br/><br/> 
+ 
+                <input  
+                    type="number"  
+                    name="age"  
+                    placeholder="Enter the Age" 
+                /> 
+ 
+                <br/><br/> 
+ 
+                <button type="submit">Submit</button> 
+ 
+            </form> 
+        `); 
+    }); 
+ 
+    // Save student 
+    app.post("/add-student", async (req, resp) => { 
+ 
+        const { name, email, age } = req.body; 
+ 
+        const collection = db.collection("my_information"); 
+ 
+        await collection.insertOne({ 
+            name: name, 
+            email: email, 
+            age: age 
+        }); 
+ 
+        resp.send("Data saved successfully"); 
+    }); 
+    app.post('/add-student-api',async(req,resp)=>{ 
+        console.log(req.body) 
+        const {name,age,email}=req.body;
+        if(!name ||  !age || !email){
+            resp.send({message:"operation failed",success:false})
+            return false;
+        }
+        const collection=db.collection('my_information');
+        const result=await collection.insertOne(req.body)
+        resp.send({"message":"date stored","success":true,result:result}) 
+    }); 
+    app.delete("/delete/:id", async (req, resp) => {
+
+    console.log(req.params.id);
+
+    const collection = db.collection("my_information");
+
+    const result = await collection.deleteOne({
+        _id: new ObjectId(req.params.id)
+    });
+
+    if (result.deletedCount === 1) {
+        resp.send({
+            message: "Student data deleted",
+            success: true
+        });
+    } else {
+        resp.send({
+            message: "Student data not deleted / ID not found",
+            success: false
+        });
+    }
+});
+ 
+}); 
+ 
+app.listen(3200, () => { 
+    console.log("Server running on port 3200"); 
+}); 
