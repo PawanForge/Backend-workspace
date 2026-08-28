b74d48131743c1eeffbce0c332e187f0d3fbf40e
Pawan kumar Yadav <pawan902670@gmail.com>
2026-08-28 20:02:14 +0530

    Demo of Mongoose

---
+++ Express/Code/Mongoose/Demo.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+async function dbConnection() {
+    await mongoose.connect("mongodb://localhost:27017/school");
+
+    const schema = mongoose.Schema({
+        name: String,
+        email: String,
+        age: Number
+    });
+
+    const studentsModel = mongoose.model("students", schema);
+
+    const result = await studentsModel.find();
+
+    console.log(result);
+}
+
+dbConnection();
